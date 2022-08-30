8194bebbc3e9afaab91df8273606d7b62f586279
munifnagi <46773316+MunifNagi@users.noreply.github.com>
2022-08-30 18:47:48 -0400

    using reflection to override flight airplane

--- src/main/java/com/solvd/airport/classes/entity/Airlines.java
+++ src/main/java/com/solvd/airport/classes/entity/Airlines.java
@@ -1,14 +1,21 @@
 package com.solvd.airport.classes.entity;
 
+import com.solvd.airport.classes.service.Booking;
 import com.solvd.airport.classes.service.Display;
 import com.solvd.airport.interfaces.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.lang.reflect.Field;
 import java.util.*;
 
 public class Airlines implements IFlights, IAirPlanes {
     private ArrayList<Flight> flights;
     private ArrayList<AirPlane> airPlanes;
     private String name;
+
+    private static final Logger logger = LogManager.getLogger(Airlines.class);
+
 
     public Airlines(String Airlines) {
         this.name = Airlines;
@@ -31,11 +38,29 @@ public class Airlines implements IFlights, IAirPlanes {
     }
 
     public void addFlight(Flight flight, ArrayList<Flight> flights) {
-        flights.add(flight);
+        this.flights.add(flight);
     }
 
     public void addAirPlane(AirPlane plane) {
         this.airPlanes.add(plane);
     }
 
+    public void overrideFlightPlane( Flight flight, AirPlane newAirPlan) {
+        if(flight.getAirPlane().getBooked() > 0) {
+            logger.warn("Flight Airplane was not changed");
+            System.out.println("This Airplane has booked seats already, you can not modify the flight airplane at this time");
+        } else {
+            Field[] flightFields = flight.getClass().getDeclaredFields();
+            Arrays.stream(flightFields)
+                    .filter(field -> field.getName() == "airPlane")
+                    .forEach(plane -> {
+                        plane.setAccessible(true);
+                        try {
+                            plane.set(flight, newAirPlan);
+                        } catch (IllegalAccessException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        }
+    }
 }
